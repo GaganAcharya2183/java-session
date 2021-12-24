07f0908610594629b6089996af7d5c679c2208a4
GaganAcharya2183 <gaganacharya@outlook.com>
2021-12-24 08:29:00 -0800

    fourth Commit

--- src/Day1/welcome.java
+++ src/Day1/welcome.java
@@ -8,34 +8,30 @@ public class welcome {
         System.out.println("Please enter your name");
         String name = scanner.next();
 
-
         System.out.println("__________________________________________________________");
         System.out.println("Hello " + name + "\n" + hello);
-        System.out.println("__________________________________________________________");
-
         System.out.println("This app performs multiplications");
         System.out.println("__________________________________________________________");
 
-        System.out.println("Please enter the first number");
-        //getting input for first number and storing it
-        double num1 = scanner.nextDouble();
-        System.out.println("Please enter the first number");
-        //getting input for second number and storing it
-        double num2 = scanner.nextDouble();
+        System.out.println("Please enter the number of multiplication you want to perform. ");
+        int num= scanner.nextInt();
         System.out.println("__________________________________________________________");
-        double result = num1*num2;
+        for(int i=0; i<num; i++) {
 
-        System.out.println("The result of " +num1+ " multiplied by " +num2+ " is :"+ result);
-        System.out.println("__________________________________________________________");
+            System.out.println("Please enter the first number");
+            //getting input for first number and storing it
+            double num1 = scanner.nextDouble();
+            System.out.println("Please enter the first number");
+            //getting input for second number and storing it
+            double num2 = scanner.nextDouble();
+            System.out.println("__________________________________________________________");
+            double result = num1 * num2;
+
+            System.out.println("The result of " + num1 + " multiplied by " + num2 + " is :" + result);
+            System.out.println("__________________________________________________________");
+        }
         System.out.println("Thank you for using the App\n" + "Have a good day");
         System.out.println("__________________________________________________________");
-
-
-
-
-
-
-
 
 
     }
