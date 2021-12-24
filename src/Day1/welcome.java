cbbae6c5c328ee70a0eae2964d54b481a24c08b0
GaganAcharya2183 <gaganacharya@outlook.com>
2021-12-24 08:12:55 -0800

    Third Commit

--- src/Day1/welcome.java
+++ src/Day1/welcome.java
@@ -1,17 +1,41 @@
 package Day1;
 import java.util.Scanner;
-import java.util.Scanner;
 
 public class welcome {
     public static void main(String[] args) {
-        String hello = "Welcome to the my demo App";
+        String hello = "Welcome to my demo App";
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your Name");
+        System.out.println("Please enter your name");
         String name = scanner.next();
+
 
         System.out.println("__________________________________________________________");
         System.out.println("Hello " + name + "\n" + hello);
         System.out.println("__________________________________________________________");
+
+        System.out.println("This app performs multiplications");
+        System.out.println("__________________________________________________________");
+
+        System.out.println("Please enter the first number");
+        //getting input for first number and storing it
+        double num1 = scanner.nextDouble();
+        System.out.println("Please enter the first number");
+        //getting input for second number and storing it
+        double num2 = scanner.nextDouble();
+        System.out.println("__________________________________________________________");
+        double result = num1*num2;
+
+        System.out.println("The result of " +num1+ " multiplied by " +num2+ " is :"+ result);
+        System.out.println("__________________________________________________________");
+        System.out.println("Thank you for using the App\n" + "Have a good day");
+        System.out.println("__________________________________________________________");
+
+
+
+
+
+
+
 
 
     }
