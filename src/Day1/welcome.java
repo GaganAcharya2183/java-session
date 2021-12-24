ab99441ba026c9ad1158c9e9fe2a7bd0e02b6859
GaganAcharya2183 <gaganacharya@outlook.com>
2021-12-24 07:55:11 -0800

    Second Commit

--- src/Day1/welcome.java
+++ src/Day1/welcome.java
@@ -9,33 +9,9 @@ public class welcome {
         System.out.println("Please enter your Name");
         String name = scanner.next();
 
+        System.out.println("__________________________________________________________");
         System.out.println("Hello " + name + "\n" + hello);
         System.out.println("__________________________________________________________");
-        System.out.println("About Me");
-        System.out.println("________________________________________________________");
-
-        System.out.println("I am Gagan Acharya");
-        System.out.println("I am 26 years old\n" + "My height is 5.7");
-        System.out.println("I live in California");
-
-        System.out.println("__________________________________________________________");
-
-        System.out.println("About the President of United States of America");
-        System.out.println("__________________________________________________________");
-        System.out.println("The President of USA is Joe Biden");
-        System.out.println("He is 65 years old\n" + "and his height is 5.10");
-        System.out.println("He lives in washington DC");
-        System.out.println("__________________________________________________________");
-
-        System.out.println("About the Prime Minister of Nepal");
-        System.out.println("__________________________________________________________");
-        System.out.println("His Name is Sher Bahadur Deuba");
-        System.out.println("He is 70 years old\n" + "His height is 5.7");
-        System.out.println("He lives in Baluwatar");
-
-        System.out.println("__________________________________________________________");
-
-
 
 
     }
